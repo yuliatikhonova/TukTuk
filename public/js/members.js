d9b224b220af5b2b7e23170c44d407500efdaff7
Michael Farris <farris.michael.j@gmail.com>
2020-07-13 22:24:24 -0700

    added modal with file input and rich text editor

--- public/js/members.js
+++ public/js/members.js
@@ -5,3 +5,17 @@ $(document).ready(() => {
     $(".member-name").text(data.email);
   });
 });
+
+
+$("#myEditor").summernote({
+  height:420,
+  toolbar: [
+    // [groupName, [list of button]]
+    ['style', ['bold', 'italic', 'underline', 'clear']],
+    ['font', ['strikethrough', 'superscript', 'subscript']],
+    ['fontsize', ['fontsize']],
+    ['color', ['color']],
+    ['para', ['ul', 'ol', 'paragraph']],
+    ['height', ['height']]
+  ]
+});

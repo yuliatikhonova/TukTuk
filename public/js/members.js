83aaeefb1cd4d9729d34a9b8b71b5f0087b9debc
yulia tikhonova <yuliatikhonova@outlook.com>
2020-07-14 17:11:16 -0700

    Got the city adding to the list to work

--- public/js/members.js
+++ public/js/members.js
@@ -1,34 +1,64 @@
 $(document).ready(() => {
+
+  const $newItemInput = $("#Add-city");//the city input
+  const $todoContainer = $(".todo-container");// where to put the list...have to figure it out
+
+  $(document).on("click", "#bucketlist", insertPost);
+
+  let posts = [];
+
+  getPost();
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
   });
-  $(document).on("click", "#bucketlist", sendData);
 
-  let $newItemInput = $("#Add-city");
-
-  function sendData() {
-    console.log("Hello There")
-
-    $.post("/api/posts", add){
-      let add = {
-        city: $newItemInput.val().trim()
-
-      };
-    };
+//=============================================================================================== Adding the city on the list
+  //adding the rows/list of data
+  function initializeRows() {
+    $todoContainer.empty();
+    let rowsToAdd = [];
+    for (let i = 0; i < posts.length; i++) {
+      rowsToAdd.push(createNewRow(posts[i]));
+    }
+    $todoContainer.prepend(rowsToAdd);
   };
+
+  // This function grabs posts from the database and updates the view
+  function getPost() {
+    $.get("/api/posts", function (data) {
+      console.log(data);
+      
+      posts = data;//push the data to the array
+      initializeRows();
+    });
+  };
+
+  function createNewRow(post) {
+    let $newInputRow = $(
+      [
+        "<li class='list-group-item'>",
+        "<span>",
+        post.city,
+        "</span>",
+        "<i class='fas fa-edit ml-auto' data-toggle='modal' data-target='#exampleModal'></i></li>"
+      ].join("")
+    );
+
+    $newInputRow.data("post", post);
+    
+    return $newInputRow;
+  };
+
+  function insertPost(event) {
+    event.preventDefault();
+    let post = {
+      city: $newItemInput.val().trim()
+    };
+
+    $.post("/api/posts", post, getPost);
+    $newItemInput.val("");
+  };
+
 });
-
-
-/*
-function getPost() {
-  let bucketlist = document.getElementById("bucketlist").innerHTML +=
-  document.getElementById("bucketlist").appendChild(li);
-    <ul id="bucketlist"></ul>
-}
-<input type="text" name="" id="search-history"></input>
-
-
-<div><button onclick="addToList()" type="button" name="button" id="bucketlist">add</button></div>
-*/

54e40105370cd10f7afb363646cf42e32021ad46
RadioJackie <jacobpeters628@gmail.com>
2020-07-15 19:38:32 -0700

    Made a darkmode and linked the signup button on the signup page to the bottom signup

---
+++ public/js/darkmode.js
@@ -0,0 +1,35 @@
+// if (localStorage.getItem("switch") == "light") {
+//     if (jQuery("body").hasClass("dark")) {
+//         jQuery("body").removeClass("dark");
+//         jQuery(".inner-switch").text("OFF");
+//     }
+// }
+// else if (localStorage.getItem("switch") == "dark") {
+//     jQuery("body").addClass("dark");
+//     jQuery(".inner-switch").text("ON");
+// }
+
+// else {
+//     if (jQuery("body").hasClass("dark")) {
+//         jQuery("body").removeClass("dark");
+//         jQuery(".inner-switch").text("OFF");
+//     }
+// }
+
+// jQuery(".inner-switch").on("click", function () {
+//     if (jQuery("body").hasClass("dark")) {
+//         jQuery("body").removeClass("dark");
+//         jQuery(".inner-switch").text("OFF");
+//         localStorage.setItem("switch", "light")
+
+//     } else {
+//         jQuery("body").addClass("dark");
+//         jQuery(".inner-switch").text("ON");
+//         localStorage.setItem("switch", "dark")
+
+//     }
+// });
+
+// $(function () {
+//     $('#darkMode').bootstrapToggle();
+// });
